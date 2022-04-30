fc6a8fb911ad524642c7bf393c596317a2459120
chika83 <chikashige.8.3@gmail.com>
2022-04-30 16:12:09 +0900

    memolist edit

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -7,6 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 import { shape, string, instanceOf, arrayOf } from 'prop-types';
 
 // import Icon from './Icon';
+import { dateToString } from '../utils';
 
 export default function MemoList(props) {
   const { memos } = props;
@@ -16,11 +17,11 @@ export default function MemoList(props) {
     return(
       <TouchableOpacity
             style={styles.memoListItem}
-            onPress={() => { navigation.navigate('MemoDetail'); }}
+            onPress={() => { navigation.navigate('MemoDetail', { id: item.id }); }}
           >
             <View>
               <Text sytle={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
-              <Text sytle={styles.memoListItemDate}>{String(item.updateAt)}</Text>
+              <Text sytle={styles.memoListItemDate}>{dateToString(item.updateAt)}</Text>
             </View>
             <TouchableOpacity
               style={styles.memoDelete}
